fcbd904d2e6c8752d1e0e5e7bc4f721513b813c3
Ashok Sharma <72646761+07et842@users.noreply.github.com>
2022-07-19 12:50:39 +0530

    added check_for_triplet declaration
    <stdlib.h> is added to support exit(0) function.

--- printgrid.h
+++ printgrid.h
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
 void printGrid(int *array, int grid_index);
 void update_array(int *array, int *array_index, int num);
 int check_array(int *array, int value);
+int check_for_triplet(int *array);
