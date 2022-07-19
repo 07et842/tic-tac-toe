f66e7adb302c872458d1ecf2fc4327288e9001cf
Ashok Sharma <72646761+07et842@users.noreply.github.com>
2022-07-19 10:27:11 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,16 +4,15 @@
 int main()
 {
     int input = 0, array[9] = {0}, array_index = 0;
-    printGrid(0);
+    printGrid(array, 0);
     for(int i = 0; i < 9; i++){
         printf("\r\nenter value:");
         scanf("%d", &input);
         if(input){
             system("clear");
-            printGrid(input);
+            printGrid(array, input);
             update_array(array, &array_index, input);
         }
     }
-
     return 0;
 }
