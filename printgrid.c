36427a78652bafe9e554a5b911413f6a71d559c4
Ashok Sharma <72646761+07et842@users.noreply.github.com>
2022-07-21 10:51:42 +0530

    final code

--- printgrid.c
+++ printgrid.c
@@ -1,7 +1,4 @@
 #include "printgrid.h"
-#define DEBUG grid[i]
-#define PLAY i+1
-#define VIEW DEBUG
 int grid[9] = {6, 7, 2, 1, 5, 9, 8, 3, 4};
 int user_array[5] = {0}, cpu_array[4] = {0};
 
@@ -9,13 +6,12 @@ void printGrid(){
     system("clear");
     printf("\r");
     for(int i = 0; i < 9; i++){
-        //use "grid[i]"" instead of "i+1" to see actual matrix
-        if(check_value_in_array(user_array, VIEW)){
+        if(check_value_in_array(user_array, grid[i])){
             printf (" x ");
-        } else if(check_value_in_array(cpu_array, VIEW)){
+        } else if(check_value_in_array(cpu_array, grid[i])){
             printf (" o ");
         } else {
-            printf(" %d ", VIEW);
+            printf(" %d ", grid[i]);
         }
         if(i < 8){
             if((i+1) % 3 == 0){
@@ -25,12 +21,24 @@ void printGrid(){
             }
         }
     }
+#if 0
+    printf("\r\nusr: ");
+    int i = 0;
+    while(user_array[i]){
+        printf("%d ", user_array[i++]);
+    }
+    printf("\r\ncpu: ");
+    i = 0;
+    while(cpu_array[i]){
+        printf("%d ", cpu_array[i++]);
+    }
+#endif
 }
 
 int update_array(int *array, int *array_index, int num)
 {
     int ret_val = num;
-    if(check_value_in_array(array, num)){
+    if((check_value_in_array(user_array, num)) || (check_value_in_array(cpu_array, num))){
         printf("\r\nnumber exist\r\n");
         return 0;
     } else {
@@ -58,8 +66,9 @@ int check_for_triplet(int *array)
         for(int j = i+1; j < 4; j++){
             for(int k = j+1; k < 5; k++){
                 if(array[i]+array[j]+array[k] == 15){
-                    // printf("\r\n----you won----");
-                    return 1;
+                    if(array[i] && array[j] && array[k])
+                        // printf("\r\n----you won----");
+                        return 1;
                 }
             }
         }
@@ -72,9 +81,9 @@ int check_for_possible_triplet(int *array)
     for(int i = 0; i < 3; i++){
         for(int j = i+1; j < 4; j++){
             possiblity = 15 - (array[i] + array[j]);
-            if((!check_value_in_array(array, possiblity)) && \
+            if((!check_value_in_array(user_array, possiblity)) && \
             (!check_value_in_array(cpu_array, possiblity)) &&\
-            (possiblity <= 9)){
+            (possiblity > 0) && (possiblity <= 9)){
                 // printf("\r\n -> %d", possiblity);
                 return possiblity;
             }
